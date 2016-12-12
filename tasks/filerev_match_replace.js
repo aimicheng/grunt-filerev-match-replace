105a1969eed81bd7006e73961c4d8ae94bd3d143
cuifan <fancui@sohu-inc.com>
2016-12-12 18:18:26 +0800

    兼容windows 分隔符，思路将输入全部转化为linux的分隔符/

--- tasks/filerev_match_replace.js
+++ tasks/filerev_match_replace.js
@@ -57,9 +57,8 @@ module.exports = function(grunt) {
                             replace_item,
                             real_url = normolize_url(item.real_url ? item.real_url : item.origin_url, current_path);
 
-                        real_url=real_url.replace(/\\/g, '/');
-                        real_url=real_url.replace(/\/\//g, '/');
-                        var rooturl='/'+options.webroot.replace(/\//g,'').replace(/\\/g,'');
+                        real_url = real_url.replace(/\\/g , '/');
+                        var rooturl='/' + options.webroot.replace(/\//g , '').replace(/\\/g , '');
                         real_url=real_url.replace(rooturl, '');
                         replace_item = url_map[real_url];
 
